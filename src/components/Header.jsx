c0159e16954dfa296917555c8b8afe8d444276a7
Kamran <Yousef.shaikhan@gmail.com>
2025-08-31 13:45:16 +0100

    created react vite project- first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../assets/logo.jpg'
+
+const Header = () => {
+  return (
+    <header id="header">
+      <img src={logo}/>
+      <h1>Meet Your Finnacial Investment</h1>
+    </header>
+  );
+};
+
+export default Header;
